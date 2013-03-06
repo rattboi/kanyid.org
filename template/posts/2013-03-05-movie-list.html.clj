a342882234255d432143a18f09d9541ffe4c3adc
rattboi <rattboi@laptop>
2013-03-05 17:20:54 -0800

    Added movie post

---
+++ template/posts/2013-03-05-movie-list.html.clj
@@ -0,0 +1,14 @@
+; @layout post
+; @title  Movie List
+; @tag    personal
+
+(p "Well, this is my first more personal post on the site. It's just a short list of movies that I am planning to watch with a few friends from school.")
+
+(ul (list (link "Freddie Got Fingered" 
+                "http://www.imdb.com/title/tt0240515/")
+          (link "Legend of Drunken Master" 
+                "http://www.imdb.com/title/tt0111512/")
+          (link "Black Dynamite" 
+                "http://www.imdb.com/title/tt1190536/?ref_=sr_1")
+          
+          ))

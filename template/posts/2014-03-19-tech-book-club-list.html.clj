cce54e98415732f6e09ca90f5ac87fffa50e72db
Jason Owen <jason.a.owen@gmail.com>
2014-04-03 10:02:26 -0700

    add author and direct link to tech book list
    Instead of showing a generic Amazon search link following a book, make
    the book title a direct, specific link. Also show the authors of the
    book to give credit and disambiguate.

--- template/posts/2014-03-19-tech-book-club-list.html.clj
+++ template/posts/2014-03-19-tech-book-club-list.html.clj
@@ -5,11 +5,18 @@
 (defn been-read [book]
  [:span {:style "text-decoration: line-through"} book])
 
-(defn book [title]
-  (list title " " (link "Amazon" (str "http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Dstripbooks&field-keywords=" title))))
+(defn book [title author url]
+  (list (link title url) ", by " author))
 
 (p "Over the summer, some friends and I are planning on reading some computer texts that are considered required reading. I'll be adding to the post as we come up with other interesting books.")
 
-(ul (list (book "Structure and Interpretation of Computer Programs")
-          (book "The Art of UNIX Programming")
-          (book "Gödel, Escher, Bach: An Eternal Golden Braid")))
+(ul (list (book "Structure and Interpretation of Computer Programs"
+                "Harold Abelson and Gerald JaySussman with Julie Sussman"
+                "http://mitpress.mit.edu/sicp/")
+          (book "The Art of UNIX Programming"
+                "Eric S. Raymond"
+                "http://www.catb.org/~esr/writings/taoup/")
+          (book "Gödel, Escher, Bach: An Eternal Golden Braid"
+                "Douglas R. Hofstadter"
+                "http://www.amazon.com/dp/0465026567")
+))

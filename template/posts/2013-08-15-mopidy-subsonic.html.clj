7b4d7c5d980320f431e5960d0dedeced22816c67
rattboi <rattboi@gmail.com>
2014-04-04 00:39:50 -0700

    Typo in mopidy-subsonic post

--- template/posts/2013-08-15-mopidy-subsonic.html.clj
+++ template/posts/2013-08-15-mopidy-subsonic.html.clj
@@ -2,4 +2,4 @@
 ; @title  Mopidy-Subsonic
 ; @tag    mopidy subsonic cli programming
 
-(p "More info can be found at the " (link "Mopidy-Subsonic" "https://github.com/rattboi/mopidy-subsonic") "Github page.")
+(p "More info can be found at the " (link "Mopidy-Subsonic" "https://github.com/rattboi/mopidy-subsonic") " Github page.")

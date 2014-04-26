f29dcebdee878b16a623ec7df2942053ee84a6a2
Bradon Kanyid <rattboi@gmail.com>
2014-04-25 18:32:35 -0700

    Adding a thing so deploys don't happen from branches

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+
+if [ "${TRAVIS_BRANCH}" != "master" ];then
+echo "Not master, not publishing";
+    exit 0;
+fi
+
 bin/expect-rsync.sh | grep -v spawn | grep -v RSA | grep -v Password

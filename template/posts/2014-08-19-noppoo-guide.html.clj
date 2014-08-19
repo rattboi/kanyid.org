bdf31cc1731c80f27c8990e53bcdd502f6015c65
Bradon Kanyid <rattboi@gmail.com>
2014-08-19 15:25:48 -0700

    Beginning of Noppoo article

---
+++ template/posts/2014-08-19-noppoo-guide.html.clj
@@ -0,0 +1,5 @@
+; @layout post
+; @title  Noppoo Choc Mini Guide
+; @tag    keyboard mechanical
+
+(p "Well, I finally did it. I bought a mechanical keyboard from " (link "Massdrop" "https://massdrop.com") ".")

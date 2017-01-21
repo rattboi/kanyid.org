0b2b67eba8a69ddcb9f92281a34bcf37b1dc95a3
Bradon Kanyid <rattboi@gmail.com>
2017-01-21 00:20:45 -0800

    New post: 2017 Resolutions. Disable travis deploys
    The travis deploys are disabled because I don't have confidence in the
    builds working from travis anymore. I'll re-enable them once I spend
    more time getting them working well.

---
+++ template/posts/2017-01-20-new-years-resolution.html.clj
@@ -0,0 +1,57 @@
+; @layout post
+; @title  2017 New Years Resolutions 
+; @tag     
+
+(h1 "Happy 2017!")
+(p "Wow, it's really been like two and a half years since I've made a new post. That's no good. Oh well, can't change it now. It's a new year, and I've got a bunch of (hopefully realistic) resolutions, one of which is to blog more. As a way of getting back into it, I'm just going to write about my resolutions, and start from there. I believe my readership is like zero, so this is mostly for personal reasons, but hopefully next year I can come back to this and be happy that at least 50% were accomplished. We shall see...")
+
+(p "Here they are, in no specific order:")
+
+(h2 "Blog More")
+
+(p "At the start of this post, I mentioned one of the resolutions would be to blog more. This is multi-faceted for me. When I say 'blog more', I'm actually hoping to:")
+(ul (list 
+      "Write More"
+      "Make a *better* blog"
+      "Put Myself Out There"))
+
+(h3 "Write More")
+(p "Writing for fun has never really been my thing, and I've always had a hard time writing anything of any length. This might be because I am concise, or it could be that I lack detail in my writing. I *suspect* that writing more often will help me build the skill of differentiating the two.")
+
+(h3 "Make a Better blog")
+(p "This blogging engine (misaki) is really showing its age. I picked it because I wanted a static site generator written in Clojure, but I didn't think about the fact that the site was going to last years and years. At this point, Misaki is abandoned, and it's going to be harder and harder to keep this working. Besides, I wanna learn more about modern web development (as I mention below), so redoing the site will be a solid step in that path.")
+
+(h3 "Put Myself Out There")
+(p "According to the personality tests I've done, I sit somewhere right on the border of extroverted and introverted. Does that just make me...verted? I generally don't have a problem with crowds or people, but I don't enjoy presenting or speaking in front of them (I guess that a lot of people probably feel this way). I also generally feel like I don't have a lot of knowledge that others don't have, and so I don't have anything worthwhile to present about.")
+
+(p "This is a likely excuse so I don't have to put in the work or put myself through the stress of sharing what I've learned. Obviously, the blogging forum works fairly well for me because I don't think anyone is actually reading it, in general. This can be a step towards my later goal of speaking at a conference.")
+
+(h2 "Start Graduate School")
+(p "I've wanted to go back to school and get my Master's degree for about a year now, but finding the time in the midst of having our first child and buying a house meant that this had to sit on the back burner. Now, with my new position at IBM, I have the opportunity to have the costs subsidized, and I also have a flexible schedule and more time in general because I work from home.")
+(p "I've actually already gotten started on this goal, and have been re-admitted to Portland State University in order to start taking Post-Bac classes towards a Masters in Computer Science. The post-bac work is necessary because I'm changing my major from Computer Engineering. I plan to start my first class in Spring 2017, with the hopes of starting the Masters program in either Spring or Fall 2018.")
+
+(h2 "Run a 10K")
+(p "Prior to having my daughter, I was running at least 3 times a week, and was able to run a 5K on a treadmill without issue. I ran in a few 5Ks with my friend Eric, and it was a good time.")
+(p "Now that we have a treadmill at home, I hope to get back to where I was before. Getting back to where I have already been actually doesn't seem like it should be very hard, so my actual goal is to progress further and run in a 10K event sometime before the end of the year.")
+
+(h2 "Learn Modern Web Development")
+(p "This is just an area that I'm interested in, because there's a lot of churn, and some neat things seem to be coming out of this area. I'd like to learn more about React, Redux, and ES6/7, as these seem to be pretty standard modern technologies, and the basis of what modern web frontend developers are using. ")
+(p "Once I have a good grasp of these, I'd like to further experiment with their Clojure counterparts, such as Reagant (which I've used a bit), and re-frame.") 
+
+(h2 "Learn Rust")
+(p "Rust is a language I've been interested in for a while now, but haven't spent the time to learn. I haven't dealt with a language with a really strong type system on a regular basis, especially for systems programming. I started my programming life as a C guy, and stuck with it for a really long time. At this point, I haven't written much C in quite a while either, and when I see all that goes into writing safe C code, I don't really want to go back to those kinds of projects.")
+(p "I've already gotten started on this goal, by reading a majority of the old Rust book, and reading some of the new Rust book as well. I have a C project in mind that a friend wrote that I'd like to convert to Rust. We'll see how that goes.")
+
+(h2 "Present at a Conference or Meetup")
+(p "Now that I've started a career with IBM, I've been experiencing a lot of firsts. I now work on a very experienced team of open-source developers for the first time, I'm working fully remotely for the first time, and I'm working on projects with larger scale for the first time. To be honest, it's pretty intimidating. I love being surrounded (virtually) by such next-level co-workers, but I'm not used to being out-engineered as often as I am.")
+(p "I can either let my imposter syndrome take over, which is the past to failure, or I can attempt to level up myself, and be the kind of co-worker that others feel excited to work with. I feel that part of the path to this is to speak at a conference of some sort. The ultimate goal would be to feel comfortable speaking about something I understand well to a large group of people. This year, though, I'd settle for speaking about something I know *somewhat* about, to a handful of people. Something on the scale of a meetup sounds scary, but not so scary that I'm gonna have nightmares about it. This goal feels like a stretch, but we'll see.")
+
+(h2 "Create Atari 2600 Game")
+(p "Ok, so I've wanted to do this for the last few years. With all the other goals on here, I'm willing to bet this gets pushed off and not done this year. I want it to be here anyways. Basically, the goal is to write a playable *not crappy* version of Tetris Attack for the Atari 2600.")
+
+(h2 "Learn more about Machine Learning")
+(p "If you read the Internet technology news like I do (wow, I'm so cool), you'll see terms like Deep Learning and Convolutional Neural Networks regularly, and if you're anything like me, then you don't know what the hell any of it means. I hope to rectify that somewhat this year, as the little I do know is extremely interesting, and I feel like the last few years have shown that ML/AI is the most up-and-coming field within Computer Science. I think IBM and Watson agree with me.")
+(p "This goal is probably the most hand-wavey of them all, because if I were to read a few articles, I could technically scratch this off the list. I don't know what my goal is, except to have fewer unknowns when it comes to ML and AI.")
+
+(h1 "Conclusion")
+(p "Thus concludes this article about the things I want to get done this year. This list is by no means definitive, but it gives an idea of the things I'm interested in, and anyways, I just wrote a bunch of words. I've alread blogged more in 2017 than I did in 2015 or 2016! I don't know how to finish this thing. Bye.")

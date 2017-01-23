02c83521f78bf5d43a1d258fb7ddac1d3c0b8485
Bradon Kanyid <rattboi@gmail.com>
2017-01-22 22:38:35 -0800

    Remove old deploy scripts used by misaki
    These scripts don't make sense anymore, because I'm able to consolidate
    all the deploy code into the travis config now.

--- bin/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "${TRAVIS_BRANCH}" != "master" ];then
-echo "Not master, not publishing";
-    exit 0;
-fi
-
-bin/expect-rsync.sh | grep -v spawn | grep -v RSA | grep -v Password
--- bin/expect-rsync.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/expect -f
-set timeout -1
-spawn rsync -e "ssh -o StrictHostKeyChecking=no" -arvuz public_html $env(DEPLOY_USERNAME)@$env(DEPLOY_HOST):$env(DEPLOY_REMOTEDIR)
-expect -exact "$env(DEPLOY_USERNAME)@$env(DEPLOY_HOST)'s password: "
-send -- "$env(DEPLOY_PASSWORD)\r"
-expect {\$\s*} { interact }
